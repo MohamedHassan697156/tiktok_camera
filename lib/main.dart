0e9374847bd86af85d1a7092b4ebbe21f51a1f66
MohamedHassan697156 <gh5093351@gmail.com>
2026-07-27 14:05:14 +0300

    commit massage

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'app/app.dart';
-
+//dfd
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
